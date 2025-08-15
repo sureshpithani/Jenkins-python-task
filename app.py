f4da5415d3e0ab9627801819d0da806912c1404b
Venkata Suresh Pitani <spitani@msa.miraclesoft.com>
2025-08-15 16:36:31 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def main():
+    print("Hello from Python App in Docker!")
+    print(f"Current time is: {datetime.now()}")
+
+if __name__ == "__main__":
+    main()
