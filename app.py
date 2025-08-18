473d3e889d81c598486091ac0ba41dcec242d691
Venkata Suresh Pitani <spitani@msa.miraclesoft.com>
2025-08-18 18:26:18 +0530

    new py commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def main():
-    print("Hello This is Suresh Pithani")
+    print("Hello, This is Suresh Pithani")
     print(f"Current time is: {datetime.now()}")
 
 if __name__ == "__main__":
